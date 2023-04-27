d407009618c22306386c3a1c00e44ecab69dd90f
Ander <106649207+Andergutobi@users.noreply.github.com>
2023-04-27 17:53:10 +0200

    Update 7-reverseList.py

--- 30 day challenge/7-reverseList.py
+++ 30 day challenge/7-reverseList.py
@@ -1,2 +1,10 @@
 #_______Write a function that reverses a list of strings.
+wordList = input('Enter some words: ').split()
 
+def reversedList(wordList):
+    r = list(wordList)
+    r.reverse()
+    return str(r)
+
+
+print(reversedList(wordList))
